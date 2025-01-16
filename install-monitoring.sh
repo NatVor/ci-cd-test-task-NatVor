0bcbd2d28f61a5facf80be1f8d6ed280f4c15151
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2025-01-16 15:21:21 +0200

    Update install-monitoring.sh

--- install-monitoring.sh
+++ install-monitoring.sh
@@ -1,1 +1,17 @@
+# Node Exporter
+sudo apt-get install prometheus-node-exporter
+sudo systemctl start prometheus-node-exporter
+sudo systemctl enable prometheus-node-exporter
+
+# cAdvisor
+docker run -d \
+  --name=cadvisor \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=5001:8080 \
+  google/cadvisor:latest
+
+ 
 
